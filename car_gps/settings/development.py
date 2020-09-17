90a2df5576ce6eae290c6cdf6c9cd4310f8431c2
phongtran0715 <phongtran0715@gmail.com>
2020-09-17 14:09:55 +0700

    Set multiple setting

--- car_gps/settings/development.py
+++ car_gps/settings/development.py
@@ -6,6 +6,20 @@ INSTALLED_APPS += [
     'debug_toolbar',
 ]
 
+# Database
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'car_gps',
+        'USER': 'spiderboot',
+        'PASSWORD': 'CloverDating@2019',
+        'HOST': 'clover.gotechjsc.com',
+        'PORT': '6688'
+    }
+}
+
 MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware', ]
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
