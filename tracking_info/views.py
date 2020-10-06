122c7381e1334e6976ba2ad1ac8d3b7b32a3f25a
phongtran0715 <phongtran0715@gmail.com>
2020-10-06 17:18:55 +0700

    Implement live tracking api

--- tracking_info/views.py
+++ tracking_info/views.py
@@ -16,9 +16,19 @@ def get_live_tracking_view(request, **kwargs):
         account = request.user
     except User.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
+    data = {}
     if request.method == 'GET':
-        obj = CarTrackingInfo.objects.filter(user_id=account.id)
-        return Response(obj)
+        info = CarTrackingInfo.objects.filter(user_id=account.id).latest('id')
+        data['response'] = {
+            "latitude": info.latitude,
+            "longitude": info.longitude,
+            "gas": info.gas,
+            "gps_status": info.gps_status,
+            "speed": info.speed,
+            "odometer": info.odometer,
+            "timestamp": info.timestamp
+        }
+        return Response(data, status=status.HTTP_200_OK)
     return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
@@ -31,8 +41,7 @@ def get_history_tracking_view(request, **kwargs):
         return Response(status=status.HTTP_404_NOT_FOUND)
     data = {}
     if request.method == 'GET':
-        tracking_data = account.tracking_info
-        data['response'] = tracking_data
+        data['response'] = "PENDING"
         return Response(data, status=status.HTTP_200_OK)
     return Response(status=status.HTTP_400_BAD_REQUEST)
 
@@ -41,14 +50,18 @@ def get_history_tracking_view(request, **kwargs):
 @permission_classes((IsAuthenticated,))
 def insert_tracking_info_view(request, **kwargs):
     try:
-        request.user
+        account = request.user
     except User.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
 
     serializer = CarTrackingSerializer(data=request.data)
     data = {}
-    if serializer.is_valid():
-        serializer.save()
-        data['response'] = "tracking information update success"
-        return Response(data, status=status.HTTP_200_OK)
+    if request.method == 'POST':
+        if serializer.is_valid():
+            car_info = serializer.data
+            account.car_info.create(latitude=car_info['latitude'], longitude=car_info['longitude'], gas=car_info['gas'],
+                                    gps_status=car_info['gps_status'], speed=car_info['speed'],
+                                    odometer=car_info['odometer'], timestamp=car_info['timestamp'])
+            data['response'] = "tracking information update success"
+            return Response(data, status=status.HTTP_200_OK)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
