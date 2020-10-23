7e3f9a8c5a71222af0b71018f63f1f057efb5f03
phongtran0715 <phongtran0715@gmail.com>
2020-10-23 09:21:30 +0700

    Change geo model field

--- tracking_info/models.py
+++ tracking_info/models.py
@@ -6,8 +6,8 @@ from rest_framework_simplejwt.state import User
 
 class CarTrackingInfo(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='car_info')
-    longitude = models.DecimalField(max_digits=9, decimal_places=6, null=True)
-    latitude = models.DecimalField(max_digits=9, decimal_places=6, null=True)
+    longitude = models.DecimalField(max_digits=20, decimal_places=17, null=True)
+    latitude = models.DecimalField(max_digits=20, decimal_places=17, null=True)
     gas = models.FloatField(blank=True, null=True)
     gps_status = models.BooleanField(blank=True, default=False)
     speed = models.FloatField(blank=True, null=True)
