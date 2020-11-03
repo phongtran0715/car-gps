8c14ecb6a1a75f32ba337d7d142af9e7c24611f7
phongtran0715 <phongtran0715@gmail.com>
2020-11-03 15:50:59 +0700

    Add django-seed package

--- car_gps/settings/base.py
+++ car_gps/settings/base.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'django_rest_passwordreset',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
+    'django_seed',
     'authentication',
     'tracking_info',
     'user_profile',
