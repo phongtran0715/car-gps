e1662a1761c8d8e02b76391a549fc04a7fe3b2ae
phongtran0715 <phongtran0715@gmail.com>
2020-11-19 17:14:26 +0700

    Custom reset password form

--- car_gps/urls.py
+++ car_gps/urls.py
@@ -14,7 +14,6 @@ urlpatterns = [
     re_path('api/(?P<version>(v1|v2))/', include('authentication.urls')),
     re_path('api/(?P<version>(v1|v2))/', include('tracking_info.urls')),
     re_path('api/(?P<version>(v1|v2))/', include('user_profile.urls')),
-    re_path('api/(?P<version>(v1|v2))/', include('settings.urls')),
     url(r'^api/password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
     re_path(r'^api/(?P<version>(v1|v2))/promotions/', api_get_promotion_view),
 
@@ -23,24 +22,17 @@ urlpatterns = [
     path('', include('home.urls')),
     path('', include('promotions.urls')),
 
-    # Password reset links (ref: https://github.com/django/django/blob/master/django/contrib/auth/views.py)
-    # path('password_change/done/',
-    #      auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'),
-    #      name='password_change_done'),
-
-    # path('password_change/', auth_views.PasswordChangeView.as_view(template_name='registration/password_change.html'),
-    #      name='password_change'),
-
     path('activate/<uidb64>/<token>/',activate_view, name='activate'),
     
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset_form.html'),
+         name='password_reset'),
+
     path('password_reset/done/',
          auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_done.html'),
          name='password_reset_done'),
 
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-
-    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset_form.html'),
-         name='password_reset'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_change.html'),
+        name='password_reset_confirm'),
 
     path('reset/done/',
          auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),
