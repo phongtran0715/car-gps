ac6c6297943d2cc7fc8da50f827640588d7a541c
phongtran0715 <phongtran0715@gmail.com>
2020-11-20 13:58:13 +0700

    Update web route

--- car_gps/urls.py
+++ car_gps/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
-from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import include, url
@@ -23,20 +22,6 @@ urlpatterns = [
     path('', include('promotions.urls')),
 
     path('activate/<uidb64>/<token>/',activate_view, name='activate'),
-    
-    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset_form.html'),
-         name='password_reset'),
-
-    path('password_reset/done/',
-         auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_done.html'),
-         name='password_reset_done'),
-
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_change.html'),
-        name='password_reset_confirm'),
-
-    path('reset/done/',
-         auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),
-         name='password_reset_complete'),
 ]
 
 # if settings.DEBUG:
