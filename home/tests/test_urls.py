f44aa29ad5cb87aba7b4a9a5a63eec900cd6b1d8
phongtran0715 <tn.phong@alticast.com>
2021-01-20 13:50:00 +0700

    Remove redundant file

--- home/tests/test_urls.py
+++
@@ -1,7 +0,0 @@
-from django.test import SimpleTestCase
-
-
-class TestUrls(SimpleTestCase):
-
-	def test_home_urls(self):
-		pass
--- home/tests/test_views.py
+++
@@ -1,7 +0,0 @@
-from django.test import SimpleTestCase
-
-
-class TestViews(SimpleTestCase):
-
-	def test_home_view(self):
-		pass
