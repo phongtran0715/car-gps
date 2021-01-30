cf7874f778d8ac49a2719deaeb08b894a2406158
bachkhoabk47 <bachkhoabk47@gmail.com>
2021-01-30 23:40:02 +0900

    Done search function for Notificatin page
    Done search function for Notificatin page

--- notifications/views.py
+++ notifications/views.py
@@ -15,18 +15,34 @@ def get_notification_view(request, *args, **kwargs):
     #context = {
     #	'notifications' : notifications
     #}
-    paginator = Paginator(notifications, 4) # Show 4 notifications per page
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
+    query = request.GET.get("q")
+    if query:
+        notifications = notifications.filter(
+            title__icontains=query
+        ).distinct()
+        paginator = Paginator(notifications, 4) # Show 25 contacts per pagepaginator = Paginator(contact_list, 25) # Show 25 contacts per page
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+  
+        def next_page_number(self):
+            return self.paginator.validate_number(self.number + 1)
 
-    def next_page_number(self):
-        return self.paginator.validate_number(self.number + 1)
+        def previous_page_number(self):
+            return self.paginator.validate_number(self.number - 1)
+        return render(request, "notifications/notifications.html", {"page_obj":page_obj})
+    else:    
+	    paginator = Paginator(notifications, 4) # Show 4 notifications per page
+	    page_number = request.GET.get('page')
+	    page_obj = paginator.get_page(page_number)
 
-    def previous_page_number(self):
-        return self.paginator.validate_number(self.number - 1)
+	    def next_page_number(self):
+	        return self.paginator.validate_number(self.number + 1)
 
-    return render(request, "notifications/notifications.html", {"page_obj":page_obj})
-	#return render(request, "notifications/notifications.html", context)
+	    def previous_page_number(self):
+	        return self.paginator.validate_number(self.number - 1)
+
+	    return render(request, "notifications/notifications.html", {"page_obj":page_obj})
+		#return render(request, "notifications/notifications.html", context)
 
 def notification_new(request):
     if request.method == "POST":
@@ -54,3 +70,11 @@ def api_get_notification_view(request, *args, **kwargs):
 			"hyperlink" : "https://www.comingsoon.net/movies/news/112239-the-amazing-spider-man-2-getting-an-imax-3d-release" 
 		}
 	return Response(data, status=status.HTTP_200_OK)
+
+def htmlspecialchars(text):
+    return (
+        text.replace("&", "&amp;").
+        replace('"', "&quot;").
+        replace("<", "&lt;").
+        replace(">", "&gt;")
+    )
