381b5764107ed5e090f83ecf4021685da6da1be0
phongtran0715 <phongtran0715@gmail.com>
2021-04-07 15:09:39 +0700

    Add admin jet template

--- car_gps/settings/base.py
+++ car_gps/settings/base.py
@@ -17,8 +17,8 @@ from rest_framework import settings
 import environ
 
 env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False)
+	# set casting, default value
+	DEBUG=(bool, False)
 )
 # reading .env file
 environ.Env.read_env()
@@ -38,56 +38,57 @@ LOGOUT_REDIRECT_URL = 'home'
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'rest_framework',
-    'django_rest_passwordreset',
-    'rest_framework_simplejwt',
-    'rest_framework_simplejwt.token_blacklist',
-    'django_seed',
-    'crispy_forms',
-    'authentication',
-    'tracking_info',
-    'user_profile',
-    'promotions',
-    'django_crontab',
-    'fcm_django',
-    'channels',
-    'notifications',
-    'bootstrap_admin',
+	'jet',
+	'django.contrib.admin',
+	'django.contrib.auth',
+	'django.contrib.contenttypes',
+	'django.contrib.sessions',
+	'django.contrib.messages',
+	'django.contrib.staticfiles',
+	'rest_framework',
+	'django_rest_passwordreset',
+	'rest_framework_simplejwt',
+	'rest_framework_simplejwt.token_blacklist',
+	'django_seed',
+	'crispy_forms',
+	'authentication',
+	'tracking_info',
+	'user_profile',
+	'promotions',
+	'django_crontab',
+	'fcm_django',
+	'channels',
+	'notifications',
+	'bootstrap_admin',
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.locale.LocaleMiddleware',
+	'django.middleware.security.SecurityMiddleware',
+	'django.contrib.sessions.middleware.SessionMiddleware',
+	'django.middleware.common.CommonMiddleware',
+	'django.middleware.csrf.CsrfViewMiddleware',
+	'django.contrib.auth.middleware.AuthenticationMiddleware',
+	'django.contrib.messages.middleware.MessageMiddleware',
+	'django.middleware.clickjacking.XFrameOptionsMiddleware',
+	'django.middleware.locale.LocaleMiddleware',
 ]
 
 ROOT_URLCONF = 'car_gps.urls'
 
 TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
+	{
+		'BACKEND': 'django.template.backends.django.DjangoTemplates',
+		'DIRS': [os.path.join(BASE_DIR, 'templates')],
+		'APP_DIRS': True,
+		'OPTIONS': {
+			'context_processors': [
+				'django.template.context_processors.debug',
+				'django.template.context_processors.request',
+				'django.contrib.auth.context_processors.auth',
+				'django.contrib.messages.context_processors.messages',
+			],
+		},
+	},
 ]
 
 WSGI_APPLICATION = 'car_gps.wsgi.application'
@@ -97,125 +98,125 @@ ASGI_APPLICATION = "car_gps.asgi.application"
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': env.db(),
+	'default': env.db(),
 }
 
 # Password validation
 PASSWORD_HASHERS = [
-    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
-    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
-    'django.contrib.auth.hashers.Argon2PasswordHasher',
-    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+	'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+	'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+	'django.contrib.auth.hashers.Argon2PasswordHasher',
+	'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
 ]
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-        'OPTIONS': {
-            'min_length': 8,
-        }
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+	{
+		'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+	},
+	{
+		'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+		'OPTIONS': {
+			'min_length': 8,
+		}
+	},
+	{
+		'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+	},
+	{
+		'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+	},
 ]
 
 REST_FRAMEWORK = {
-    # When you enable API versioning, the request.version attribute will contain a string
-    # that corresponds to the version requested in the incoming client request.
-    'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.URLPathVersioning',
-    # Permission settings
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated'
-    ],
-    # Authentication settings
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
-    ],
-    # Pagination style
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 30,
+	# When you enable API versioning, the request.version attribute will contain a string
+	# that corresponds to the version requested in the incoming client request.
+	'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.URLPathVersioning',
+	# Permission settings
+	'DEFAULT_PERMISSION_CLASSES': [
+		'rest_framework.permissions.IsAuthenticated'
+	],
+	# Authentication settings
+	'DEFAULT_AUTHENTICATION_CLASSES': [
+		'rest_framework.authentication.TokenAuthentication',
+		'rest_framework_simplejwt.authentication.JWTAuthentication',
+		'rest_framework.authentication.BasicAuthentication',
+	],
+	# Pagination style
+	'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+	'PAGE_SIZE': 30,
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': datetime.timedelta(days=90),
-    'REFRESH_TOKEN_LIFETIME': datetime.timedelta(days=90),
-    'ROTATE_REFRESH_TOKENS': False,
-    'BLACKLIST_AFTER_ROTATION': True,
+	'ACCESS_TOKEN_LIFETIME': datetime.timedelta(days=90),
+	'REFRESH_TOKEN_LIFETIME': datetime.timedelta(days=90),
+	'ROTATE_REFRESH_TOKENS': False,
+	'BLACKLIST_AFTER_ROTATION': True,
 
-    'ALGORITHM': 'HS256',
-    'SIGNING_KEY': SECRET_KEY,
-    'VERIFYING_KEY': None,
-    'AUDIENCE': None,
-    'ISSUER': None,
+	'ALGORITHM': 'HS256',
+	'SIGNING_KEY': SECRET_KEY,
+	'VERIFYING_KEY': None,
+	'AUDIENCE': None,
+	'ISSUER': None,
 
-    'AUTH_HEADER_TYPES': ('Bearer',),
-    'USER_ID_FIELD': 'id',
-    'USER_ID_CLAIM': 'user_id',
+	'AUTH_HEADER_TYPES': ('Bearer',),
+	'USER_ID_FIELD': 'id',
+	'USER_ID_CLAIM': 'user_id',
 
-    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
-    'TOKEN_TYPE_CLAIM': 'token_type',
+	'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+	'TOKEN_TYPE_CLAIM': 'token_type',
 
-    'JTI_CLAIM': 'jti',
+	'JTI_CLAIM': 'jti',
 
-    'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
-    'SLIDING_TOKEN_LIFETIME': datetime.timedelta(minutes=1),
-    'SLIDING_TOKEN_REFRESH_LIFETIME': datetime.timedelta(days=1),
+	'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+	'SLIDING_TOKEN_LIFETIME': datetime.timedelta(minutes=1),
+	'SLIDING_TOKEN_REFRESH_LIFETIME': datetime.timedelta(days=1),
 }
 
 LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'root': {
-        'level': 'INFO',
-        'handlers': ['file'],
-    },
-    'formatters': {
-        'default': {
-            'format': '%(levelname)s %(asctime)s %(message)s'
-        }
-    },
-    'handlers': {
-        'file': {
-            'level': 'INFO',
-            'class': 'logging.FileHandler',
-            'formatter': 'default',
-            'filename': os.path.join(BASE_DIR, 'log/general.log')
-        }
-    },
-    'loggers': {
-        'django': {
-            'level': 'INFO',
-            'handlers': ['file'],
-            'propagate': True,
-        }
-    }
+	'version': 1,
+	'disable_existing_loggers': False,
+	'root': {
+		'level': 'INFO',
+		'handlers': ['file'],
+	},
+	'formatters': {
+		'default': {
+			'format': '%(levelname)s %(asctime)s %(message)s'
+		}
+	},
+	'handlers': {
+		'file': {
+			'level': 'INFO',
+			'class': 'logging.FileHandler',
+			'formatter': 'default',
+			'filename': os.path.join(BASE_DIR, 'log/general.log')
+		}
+	},
+	'loggers': {
+		'django': {
+			'level': 'INFO',
+			'handlers': ['file'],
+			'propagate': True,
+		}
+	}
 }
 
 CRONJOBS = [
-    ('0 0 * * *', 'tracking_info.cron.db_rotation_job', '>> {}'.format(os.path.join(BASE_DIR, 'log/scheduled_job.log'))),
+	('0 0 * * *', 'tracking_info.cron.db_rotation_job', '>> {}'.format(os.path.join(BASE_DIR, 'log/scheduled_job.log'))),
 ]
 
 FCM_DJANGO_SETTINGS = {
-    # default: _('FCM Django')
-    "APP_VERBOSE_NAME": "[Vinatrack GPS]",
-    # Your firebase API KEY
-    "FCM_SERVER_KEY": env('FCM_SERVER_KEY'),
-    # true if you want to have only one active device per registered user at a time
-    # default: False
-    "ONE_DEVICE_PER_USER": False,
-    # devices to which notifications cannot be sent,
-    # are deleted upon receiving error response from FCM
-    # default: False
-    "DELETE_INACTIVE_DEVICES": False,
+	# default: _('FCM Django')
+	"APP_VERBOSE_NAME": "[Vinatrack GPS]",
+	# Your firebase API KEY
+	"FCM_SERVER_KEY": env('FCM_SERVER_KEY'),
+	# true if you want to have only one active device per registered user at a time
+	# default: False
+	"ONE_DEVICE_PER_USER": False,
+	# devices to which notifications cannot be sent,
+	# are deleted upon receiving error response from FCM
+	# default: False
+	"DELETE_INACTIVE_DEVICES": False,
 }
 
 EMAIL_CONFIG = env.email_url('EMAIL_URL')
@@ -240,8 +241,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-    # Extra lookup directories for collectstatic to find static files
+	os.path.join(BASE_DIR, 'static'),
+	# Extra lookup directories for collectstatic to find static files
 )
 
 MEDIA_URL = '/media/'
@@ -252,11 +253,11 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 
 CHANNEL_LAYERS = {
-    'default': {
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
-        },
-    },
+	'default': {
+		'BACKEND': 'channels_redis.core.RedisChannelLayer',
+		'CONFIG': {
+			"hosts": [('127.0.0.1', 6379)],
+		},
+	},
 }
 
--- car_gps/urls.py
+++ car_gps/urls.py
@@ -16,29 +16,30 @@ router = DefaultRouter()
 router.register(r'devices', FCMDeviceAuthorizedViewSet)
 
 urlpatterns = [
-    
-    #API route
-    re_path('api/(?P<version>(v1|v2))/', include('authentication.urls')),
-    re_path('api/(?P<version>(v1|v2))/', include('tracking_info.urls')),
-    re_path('api/(?P<version>(v1|v2))/', include('user_profile.urls')),
-    
-    path('api/password_reset/<token>', views.reset_password_view, name='api_password_reset_verify_token'),
-    url(r'^api/password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
-    
-    re_path(r'^api/(?P<version>(v1|v2))/promotions/', api_get_promotion_view),
-    re_path(r'^api/(?P<version>(v1|v2))/notifications/', api_get_notification_view),
-    re_path(r'^api/(?P<version>(v1|v2))/', include(router.urls)),
+	
+	#API route
+	re_path('api/(?P<version>(v1|v2))/', include('authentication.urls')),
+	re_path('api/(?P<version>(v1|v2))/', include('tracking_info.urls')),
+	re_path('api/(?P<version>(v1|v2))/', include('user_profile.urls')),
+	
+	path('api/password_reset/<token>', views.reset_password_view, name='api_password_reset_verify_token'),
+	url(r'^api/password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
+	
+	re_path(r'^api/(?P<version>(v1|v2))/promotions/', api_get_promotion_view),
+	re_path(r'^api/(?P<version>(v1|v2))/notifications/', api_get_notification_view),
+	re_path(r'^api/(?P<version>(v1|v2))/', include(router.urls)),
 
-    # Web route
-    path('admin/', admin.site.urls),
-    path('', views.home_screen_view, name="index"),
-    path('tin-tuc/', views.news_view, name="news"),
-    path('', include('promotions.urls')),
-    path('', include('notifications.urls')),
-    path('car/tracking/', index, name='tracking_index'),
-    path('car/tracking/<str:room_name>/', room, name='room'),
+	# Web route
+	url(r'^jet/', include('jet.urls', 'jet')),  # Django JET URLS
+	path('admin/', admin.site.urls),
+	path('', views.home_screen_view, name="index"),
+	path('tin-tuc/', views.news_view, name="news"),
+	path('', include('promotions.urls')),
+	path('', include('notifications.urls')),
+	path('car/tracking/', index, name='tracking_index'),
+	path('car/tracking/<str:room_name>/', room, name='room'),
 
-    path('activate/<uidb64>/<token>/',activate_view, name='activate'),
+	path('activate/<uidb64>/<token>/',activate_view, name='activate'),
 ]
 
 # if settings.DEBUG:
