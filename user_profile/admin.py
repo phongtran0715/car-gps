c8d4f185d519a50b2c99444a601e051a7a0dc8d6
dungnguyenbk92 <nguyendungbk92@gmail.com>
2021-04-21 17:40:42 +0700

    [create] add feature import export file

--- user_profile/admin.py
+++ user_profile/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from user_profile.models import UserProfile
+from import_export import resources
+from import_export.admin import ImportExportActionModelAdmin
+
+
+class UserProfileResource(resources.ModelResource):
+    class Meta:
+        model = UserProfile
+
+
+class UserProfileAdmin(ImportExportActionModelAdmin):
+    resource_class = UserProfileResource
+
 
 # Register your models here.
-admin.site.register(UserProfile)
+admin.site.register(UserProfile, UserProfileAdmin)
