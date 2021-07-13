71bce1241410e95ce437c84871026835de93d22e
phongtran <phongtran0715@gmail.com>
2021-07-13 17:18:37 +0700

    Implement inport/export user from admin pannel

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from rest_framework_simplejwt.state import User
+from import_export import resources
+from import_export.admin import ImportExportActionModelAdmin
 
-# Register your models here.
+
+class UserAdminResource(resources.ModelResource):
+    class Meta:
+        model = User
+        exclude = ('password', 'user_permissions')
+
+
+class UserAdmin(ImportExportActionModelAdmin):
+	resource_class = UserAdminResource
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- user_profile/admin.py
+++ user_profile/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from .models import UserProfile
+from import_export import resources
+from import_export.admin import ImportExportActionModelAdmin
 
 
-@admin.register(UserProfile)
-class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ("id", "car_name", "plate_number")
+
+class UserProfileResource(resources.ModelResource):
+    class Meta:
+        model = UserProfile
+
+class UserProfileAdmin(ImportExportActionModelAdmin):
+    resource_class = UserProfileResource
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
